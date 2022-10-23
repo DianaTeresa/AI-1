0f22a38310cc677e1fa7dda85c672b4592b6f70a
Nguyen Hoang Anh <76523655+anh-nh@users.noreply.github.com>
2022-10-23 11:55:39 +0700

    Final changes

--- main.py
+++ main.py
@@ -55,49 +55,49 @@ def level_1(maps):
         ap.visualize_maze(matrix, bonus_points, portals, start, goal, wayoutDFS, f'output\\level_1\\input{mapID}\\dfs\\dfs')
         ap.PGAME(graph, start, goal, wayoutDFS,bonus_points, portals, openDFS,dir_path,path,'dfs')
         with open(f'output\\level_1\\input{mapID}\\dfs\\dfs.txt', 'w') as file:
-            file.write(f'Cost: {len(wayoutDFS)}')
+            file.write(f'Cost: {len(wayoutDFS) - 2}')
         
         wayoutBFS, openBFS = ap.bfs(graph, start, goal)
         ap.visualize_maze(matrix, bonus_points, portals, start, goal, wayoutBFS, f'output\\level_1\\input{mapID}\\bfs\\bfs')
         path =f'\\output\\level_1\\input{mapID}\\bfs'
         ap.PGAME(graph, start, goal, wayoutBFS,bonus_points, portals, openBFS,dir_path,path,'bfs')
         with open(f'output\\level_1\\input{mapID}\\bfs\\bfs.txt', 'w') as file:
-            file.write(f'Cost: {len(wayoutBFS)}')
+            file.write(f'Cost: {len(wayoutBFS) - 2}')
 
         wayoutUCS, openUCS = ap.UCS(graph, start, goal)
         ap.visualize_maze(matrix, bonus_points, portals, start, goal, wayoutUCS, f'output\\level_1\\input{mapID}\\ucs\\ucs')
         path =f'\\output\\level_1\\input{mapID}\\UCS'
         ap.PGAME(graph, start, goal, wayoutUCS,bonus_points, portals, openUCS,dir_path,path, 'ucs')
         with open(f'output\\level_1\\input{mapID}\\ucs\\ucs.txt', 'w') as file:
-            file.write(f'Cost: {len(wayoutUCS)}')
+            file.write(f'Cost: {len(wayoutUCS) - 2}')
 
         wayoutGBFS_1, openGBFS_1 = ap.GBFS_Heur1(graph, start, goal)
         ap.visualize_maze(matrix, bonus_points, portals, start, goal, wayoutGBFS_1, f'output\\level_1\\input{mapID}\\gbfs_heuristic_1\\gbfs_heuristic_1')
         path =f'\\output\\level_1\\input{mapID}\\gbfs_heuristic_1'
         ap.PGAME(graph, start, goal, wayoutGBFS_1,bonus_points, portals, openGBFS_1,dir_path,path,'gbfs_heuristic_1')
         with open(f'output\\level_1\\input{mapID}\\gbfs_heuristic_1\\gbfs_heuristic_1.txt', 'w') as file:
-            file.write(f'Cost: {len(wayoutGBFS_1)}')
+            file.write(f'Cost: {len(wayoutGBFS_1) - 2}')
 
         wayoutGBFS_2, openGBFS_2 = ap.GBFS_Heur2(graph, start, goal)
         ap.visualize_maze(matrix, bonus_points, portals, start, goal, wayoutGBFS_2, f'output\\level_1\\input{mapID}\\gbfs_heuristic_2\\gbfs_heuristic_2')
         path =f'\\output\\level_1\\input{mapID}\\gbfs_heuristic_2'
         ap.PGAME(graph, start, goal, wayoutGBFS_2,bonus_points, portals, openGBFS_2,dir_path,path,'gbfs_heuristic_2')
         with open(f'output\\level_1\\input{mapID}\\gbfs_heuristic_2\\gbfs_heuristic_2.txt', 'w') as file:
-            file.write(f'Cost: {len(wayoutGBFS_2)}')
+            file.write(f'Cost: {len(wayoutGBFS_2) - 2}')
 
         wayoutAstar_1, openAstar_1 = ap.a_star1(graph, start, goal)
         ap.visualize_maze(matrix, bonus_points, portals, start, goal, wayoutAstar_1, f'output\\level_1\\input{mapID}\\astar_heuristic_1\\astar_heuristic_1')
         path =f'\\output\\level_1\\input{mapID}\\astar_heuristic_1'
         ap.PGAME(graph, start, goal, wayoutAstar_1,bonus_points, portals, openAstar_1,dir_path,path,'astar_heuristic_1')
         with open(f'output\\level_1\\input{mapID}\\astar_heuristic_1\\astar_heuristic_1.txt', 'w') as file:
-            file.write(f'Cost: {len(wayoutAstar_1)}')
+            file.write(f'Cost: {len(wayoutAstar_1) - 2}')
 
         wayoutAstar_2, openAstar_2 = ap.a_star2(graph, start, goal)
         ap.visualize_maze(matrix, bonus_points, portals, start, goal, wayoutAstar_2, f'output\\level_1\\input{mapID}\\astar_heuristic_2\\astar_heuristic_2')
         path =f'\\output\\level_1\\input{mapID}\\astar_heuristic_2'
         ap.PGAME(graph, start, goal, wayoutAstar_2,bonus_points, portals, openAstar_2,dir_path,path,'astar_heuristic_2')
         with open(f'output\\level_1\\input{mapID}\\astar_heuristic_2\\astar_heuristic_2.txt', 'w') as file:
-            file.write(f'Cost: {len(wayoutAstar_2)}') 
+            file.write(f'Cost: {len(wayoutAstar_2) - 2}') 
   
         mapID += 1
 
@@ -174,20 +174,20 @@ def advance(maps):
                 else:
                     adj.append(1)
             graph.append(adj)     
-        wayout, open = ap.BFS_teleport(matrix, start, goal)
+        wayout, openSet = ap.BFS_teleport(matrix, start, goal)
         ap.visualize_maze(matrix, bonus_points, portals, start, goal, wayout, f'output\\advance\\input{mapID}\\output{mapID}')
         path =f'\\output\\advance\\input{mapID}\\'
-        ap.PGAME(graph, start, goal, wayout,bonus_points, portals, open,dir_path,path,f'output{mapID}')
+        ap.PGAME(graph, start, goal, wayout,bonus_points, portals, openSet,dir_path,path,f'output{mapID}')
         with open(f'output\\advance\\input{mapID}\\output{mapID}.txt', 'w') as file:
-            file.write(f'Cost: {len(wayout)}')
+            file.write(f'Cost: {len(wayout) - 2}')
             file.close()
         mapID += 1
     pass
 
 
 
-#level_1(getInput('input\\level_1'))
+level_1(getInput('input\\level_1'))
 level_2(getInput('input\\level_2'))
-#advance(getInput('input\\advance'))
+advance(getInput('input\\advance'))
 
 
